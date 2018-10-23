4ffba4b7cc06109f74641563a039c5ed0e55ff93
Steven Foltz <stevenmasonfoltz@gmail.com>
2018-10-23 17:16:05 -0500

    Debugging variable names, troubleshooting

--- phaseset.py
+++ phaseset.py
@@ -1,7 +1,7 @@
 import pysam
 import vcf
 
-from SomaticHaplotype import PhaseSet
+from SomaticHaplotype import *
 
 ################################################################################
 # bam functions
@@ -86,57 +86,21 @@ def extract_phase_sets_from_bam(bam_filename, chr = None, start_bp = None, end_b
 # VCF functions
 ################################################################################
 
-def extract_key_from_record(record):
-  # extract the key information from pyVCF record
-  key_list = [ record.CHROM, record.start, record.end, record.REF, ",".join([ str(x) for x in record.ALT ]) ]
-  key = ':'.join( [ str(x) for x in key_list ] )
-  return(key)
 
-def extract_phase_set_from_record(record, sample_id):
-  # extract the phase set from a pyVCF record
-  chrom = record.CHROM
-  ps = record.genotype(sample_id).data.PS
-  return(str(chrom) + ":" + str(ps))
-
-def extract_phase_sets_from_vcf(vcf_filename, sample_id, 
-  chr = None, start_bp = None, end_bp = None):
+def extract_variants_from_VCF(vcf_filename, sample_id, chr = None, start_bp = None, end_bp = None):
   # build a dictionary of phase sets present in VCF
   # only includes variants that are phased heterozygotes (useful for phase-related activities)
   this_vcf = vcf.Reader( filename = vcf_filename )
-
-  phase_set_dict = {} # dictionary to hold phase set information
+  variant_dict = {} # dictionary to hold all variants
 
   for record in this_vcf.fetch( str(chr) , start_bp, end_bp ): # loop over each record in VCF
-    if phased_heterozygote(record, sample_id):
-      this_key = extract_key_from_record(record)
-      this_key += ":" + str(record.genotype(sample_id).data.GT)
-      this_ps = extract_phase_set_from_record(record, sample_id)
-      if this_ps not in phase_set_dict:
-        # initialize phase set dict as [ [list of keys], chrom, min pos, max pos, number of variants, phase_set ]
-        phase_set_dict[this_ps] = [ [this_key], record.CHROM, record.POS, record.POS, 1, this_ps]
-      else:
-        phase_set_dict[this_ps][0].append(this_key) # append variant key to list of variant keys
-        phase_set_dict[this_ps][4] += 1 # add one to number of variants associated with this phase set
-        if record.POS < phase_set_dict[this_ps][2]: # update the min position 
-          phase_set_dict[this_ps][2] = record.POS
-        elif record.POS > phase_set_dict[this_ps][3]: # update the max position
-          phase_set_dict[this_ps][3] = record.POS
-        else:
-          continue
+    this_variant = Variant(record, sample_id)
+    if this_variant.getVariantKey() in variant_dict: # check if variant already in dictionary
+      sys.exit("Whoa variant already exists in variant dictionary\n" + record)
     else:
-      continue
+      variant_dict[this_variant.getVariantKey()] = this_variant
 
-  return(phase_set_dict)
-
-def phased_heterozygote(record, sample_id):
-  # determine if pyVCF record refers to a phased heterozygote (return True)
-  # otherwise, variants are not useful for phasing
-  phased = record.genotype(sample_id).phased
-  heterozygote = record.genotype(sample_id).is_het
-  if phased and heterozygote:
-    return(True)
-  else:
-    return(False)
+  return(variant_dict)
 
 
 ################################################################################
@@ -156,21 +120,24 @@ def main(args):
   except:
     end = None
 
-  # initialize phase set dictionary
-  bam_phase_set_dictionary = extract_phase_sets_from_bam(
-    args.bam,
-    chr = chrom, 
-    start_bp = start, 
-    end_bp = end)
-
-  vcf_phase_set_dictionary = extract_phase_sets_from_vcf(
+  # bam phase set dictionary
+  #bam_phase_set_dictionary = extract_phase_sets_from_bam(
+  #  args.bam,
+  #  chr = chrom, 
+  #  start_bp = start, 
+  #  end_bp = end)
+  
+  # vcf phase set dictionary
+  vcf_variants_dictionary = extract_variants_from_VCF(
     args.vcf,
     args.vcf_id,
     chr = chrom,
     start_bp = start,
     end_bp = end)
 
-  return(bam_phase_set_dictionary)
+  return(vcf_variants_dictionary)
+
+  #return(bam_phase_set_dictionary)
 
 if __name__ == '__main__':
   main(args)
