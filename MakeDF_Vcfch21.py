1488ecf2d0491379f91ad3b03ce800117b9be46a
JessBaral <jessika.baral@gmail.com>
2018-10-30 11:24:17 -0500

    text file adapted
    make it fewer lines as a test to make sure that the text file opens but have not yet made text file

--- MakeDF_Vcfch21.py
+++ MakeDF_Vcfch21.py
@@ -30,15 +30,20 @@ variantHet_list = []
 variantHomRef_list = []
 variantHomAlt_list = []
 
+i  = 0
 for record in vcf_reader:
-    if(record.is_snp):
-        variantPosition_list.append(record.POS)
-        variantID_list.append(record.ID)
-        variantRef_list.append(record.REF)
-        variantAlt_list.append(record.ALT)
-        variantHet_list.append(record.num_het)
-        variantHomRef_list.append(record.num_hom_ref)
-        variantHomAlt_list.append(record.num_hom_alt)
+    if (i < 10000):
+        if(record.is_snp):
+            variantPosition_list.append(record.POS)
+            variantID_list.append(record.ID)
+            variantRef_list.append(record.REF)
+            variantAlt_list.append(record.ALT)
+            variantHet_list.append(record.num_het)
+            variantHomRef_list.append(record.num_hom_ref)
+            variantHomAlt_list.append(record.num_hom_alt)
+            i += 1
+    else:
+        break
 
 
 df = pd.DataFrame({'Position': variantPosition_list, 'Chrom': "21", 'ID': variantID_list, 'Reference': variantRef_list, \
@@ -48,4 +53,5 @@ df = pd.DataFrame({'Position': variantPosition_list, 'Chrom': "21", 'ID': varian
 df['PatientCount'] = (df['numberHomozygousRef'] + df['numberHomozygousAlt'] + df['numHet'])
 
 df['MutantAlleleFrequency'] = (df['numHet'] + (2*df['numberHomozygousAlt']))/(2*(df['numberHomozygousRef'] + df['numberHomozygousAlt'] + df['numHet']))
+
 
