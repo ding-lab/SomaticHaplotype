b3c944402e5a5e5336c2bc94a908ec4192e9a2cf
envest <stevenmasonfoltz@gmail.com>
2018-11-05 10:05:48 -0600

    Allow for no range argument

--- summarize.py
+++ summarize.py
@@ -17,21 +17,26 @@ from SomaticHaplotype import *
 
 def main(args):
 
-  # parse the genomic range argument
-  chrom = args.range.split(":")[0]
-  try:
-    start = int(args.range.split(":")[1].split("-")[0])
-  except:
-    start = None
-  try:
-    end = int(args.range.split(":")[1].split("-")[1])
-  except:
-    end = None
-
   # path to input pickle file
   pickle_path = args.ps1
   phase_set_dict = pickle.load(pickle_path)
   variants_dict = pickle.load(pickle_path)
+
+  # parse the genomic range argument
+  if args.range is None:
+    chrom = None
+    start = None
+    end = None
+  else:  
+    chrom = args.range.split(":")[0]
+    try:
+      start = int(args.range.split(":")[1].split("-")[0])
+    except:
+      start = None
+    try:
+      end = int(args.range.split(":")[1].split("-")[1])
+    except:
+      end = None
 
 if __name__ == '__main__':
   main(args)
