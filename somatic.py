add740c4a9ed479e7511f22ca80a7df609c6773b
Steven Foltz <stevenmasonfoltz@gmail.com>
2019-01-09 12:23:10 -0600

    Variants are stored within lists in variant dictionary

--- somatic.py
+++ somatic.py
@@ -11,32 +11,41 @@ def return_barcodes_supporting_variant(variant_key, vcf_variants_dictionary):
 
   if variant_key not in vcf_variants_dictionary:
     sys.exit("Variant " + variant_key + " not in vcf_variants_dictionary.")
+  elif len(vcf_variants_dictionary[variant_key]) > 1:
+    sys.exit("Variant " + variant_key + " has more than one VCF record.")
   else:
-    this_variant = vcf_variants_dictionary[variant_key]
+    this_variant = vcf_variants_dictionary[variant_key][0]
     if this_variant.return_Genotype() in ["1/0", "1|0"]:
       variant_allele = 0
     elif this_variant.return_Genotype() in ["0/1", "0|1"]:
       variant_allele = 1
     else:
       sys.exit("Variant " + variant_key + " is not 0/1, 1/0, 0|1, or 1|0.")
+    other_allele = (variant_allele + 1) % 2
+    molecules_supporting_this_position = list(this_variant.return_Molecules()[variant_allele].keys())
+    #molecules_supporting_this_position.extend(list(this_variant.return_Molecules()[other_allele].keys()))
+    return(molecules_supporting_this_position)
 
-    molecules_supporting_variant_allele = this_variant.return_Molecules()[variant_allele].keys()
-    return(molecules_supporting_variant_allele)
+def return_variants_covered_by_barcodes(barcode_list, base_variant_key, vcf_variants_dictionary):
 
-def return_variants_covered_by_barcodes(barcode_list, base_variant, vcf_variants_dictionary):
-  
   variant_list = []
-  
-  for this_variant in vcf_variants_dictionary:
-    if this_variant.return_VariantKey() == base_variant or (this_variant.determine_if_phased_heterozygote() and this_variant.return_Genotype() in ["0|1", "1|0"] and this_variant.return_IsSNP()):
-      barcodes_allele0 = this_variant.return_Molecules()[0].keys()
-      barcdoes_allele1 = this_variant.return_Molecules()[1].keys()
+  this_phase_set = vcf_variants_dictionary[base_variant_key][0].return_PhaseSetID()
 
-      bx_overlap_allele0 = [ bx in barcodes_allele0 for bx in barcode_list]
-      bx_overlap_allele1 = [ bx in barcodes_allele1 for bx in barcode_list]
+  for this_variant_key in vcf_variants_dictionary:
+    if len(vcf_variants_dictionary[this_variant_key]) > 1:
+      print("Variant " + this_variant_key + " has more than one VCF record.")
+    else:
+      this_variant = vcf_variants_dictionary[this_variant_key][0]
+      #if this_variant.return_VariantKey() == base_variant or (this_variant.determine_if_phased_heterozygote() and this_variant.return_Genotype() in ["0|1", "1|0"] and this_variant.return_IsSNP()):
+      if this_variant.return_IsSNP() and this_variant.return_PhaseSetID() == this_phase_set:
+        barcodes_allele0 = list(this_variant.return_Molecules()[0].keys())
+        barcodes_allele1 = list(this_variant.return_Molecules()[1].keys())
 
-      if any(bx_overlap_allele0) or any(bx_overlap_allele1):
-        variant_list.append(this_variant)
+        bx_overlap_allele0 = [ bx in barcodes_allele0 for bx in barcode_list]
+        bx_overlap_allele1 = [ bx in barcodes_allele1 for bx in barcode_list]
+
+        if any(bx_overlap_allele0) or any(bx_overlap_allele1):
+          variant_list.append(this_variant)
 
   return(variant_list)
 
@@ -81,9 +90,7 @@ def return_coverage_vector_for_variant(variant_key, barcode_list, vcf_variants_d
 def create_coverage_dictionary(variant_key, vcf_variants_dictionary):
 
   bx_supporting_variants = return_barcodes_supporting_variant(variant_key, vcf_variants_dictionary)
-  print(bx_supporting_variants)
-  #variants_covered = return_variants_covered_by_barcodes(bx_supporting_variants, vcf_variants_dictionary)
-
+  variants_covered = return_variants_covered_by_barcodes(bx_supporting_variants, variant_key, vcf_variants_dictionary)
   #coverage_dict = {}
   #coverage_dict["barcodes"].extend(bx_supporting_variants)
   
