b29b99a669a9da47abb2df0a3a6d3059527820b7
Steven Foltz <stevenmasonfoltz@gmail.com>
2019-01-09 15:35:30 -0600

    Outputs one line per combination of barcode and variant

--- somatic.py
+++ somatic.py
@@ -15,16 +15,12 @@ def return_barcodes_supporting_variant(variant_key, vcf_variants_dictionary):
     sys.exit("Variant " + variant_key + " has more than one VCF record.")
   else:
     this_variant = vcf_variants_dictionary[variant_key][0]
-    if this_variant.return_Genotype() in ["1/0", "1|0"]:
-      variant_allele = 0
-    elif this_variant.return_Genotype() in ["0/1", "0|1"]:
-      variant_allele = 1
+    if this_variant.return_Genotype() not in ["1/0", "1|0", "0/1", "0|1", "1/1", "1|1", "0/0", "0|0"]:
+      sys.exit("Variant " + variant_key + " is not 0/1, 1/0, 0|1, 1|0, 0/0, 0|1, 1/1, or 1|1.")
     else:
-      sys.exit("Variant " + variant_key + " is not 0/1, 1/0, 0|1, or 1|0.")
-    other_allele = (variant_allele + 1) % 2
-    molecules_supporting_this_position = list(this_variant.return_Molecules()[variant_allele].keys())
-    #molecules_supporting_this_position.extend(list(this_variant.return_Molecules()[other_allele].keys()))
-    return(molecules_supporting_this_position)
+      molecules_supporting_this_position = list(this_variant.return_Molecules()[0].keys()) # 0 refers to return_Molecules() dictionary {0:{ref bx}, 1:{alt bx}}
+      molecules_supporting_this_position.extend(list(this_variant.return_Molecules()[1].keys())) # 1 refers to return_Molecules() dictionary {0:{ref bx}, 1:{alt bx}}
+      return(molecules_supporting_this_position) # List of barcodes supporting either REF or ALT allele
 
 def return_variants_covered_by_barcodes(barcode_list, base_variant_key, vcf_variants_dictionary):
 
@@ -36,87 +32,94 @@ def return_variants_covered_by_barcodes(barcode_list, base_variant_key, vcf_vari
       print("Variant " + this_variant_key + " has more than one VCF record.")
     else:
       this_variant = vcf_variants_dictionary[this_variant_key][0]
-      #if this_variant.return_VariantKey() == base_variant or (this_variant.determine_if_phased_heterozygote() and this_variant.return_Genotype() in ["0|1", "1|0"] and this_variant.return_IsSNP()):
       if this_variant.return_IsSNP() and this_variant.return_PhaseSetID() == this_phase_set:
-        barcodes_allele0 = list(this_variant.return_Molecules()[0].keys())
-        barcodes_allele1 = list(this_variant.return_Molecules()[1].keys())
+        if this_variant.return_Genotype() not in ["1/0", "1|0", "0/1", "0|1", "1/1", "1|1", "0/0", "0|0"]:
+          continue
+        else:
+          barcodes_allele0 = list(this_variant.return_Molecules()[0].keys())
+          barcodes_allele1 = list(this_variant.return_Molecules()[1].keys())
 
-        bx_overlap_allele0 = [ bx in barcodes_allele0 for bx in barcode_list]
-        bx_overlap_allele1 = [ bx in barcodes_allele1 for bx in barcode_list]
+        bx_overlap_allele0 = [ bx in barcodes_allele0 for bx in barcode_list ]
+        bx_overlap_allele1 = [ bx in barcodes_allele1 for bx in barcode_list ]
 
         if any(bx_overlap_allele0) or any(bx_overlap_allele1):
           variant_list.append(this_variant)
 
   return(variant_list)
 
-def return_coverage_vector_for_variant(variant_key, barcode_list, vcf_variants_dictionary):
-
-  this_variant = vcf_variants_dictionary[variant_key]
-  coverage_vector = []
-
-  if this_variant.return_Genotype() == "0|1":
-    variant_allele = 1
-  elif this_variant.return_Genotype == "1|0":
-    variant_allele = 0
+def return_allele_supported_by_barcode(barcode, variant_key, vcf_variants_dictionary):
+  if variant_key not in vcf_variants_dictionary:
+    sys.exit("Variant " + variant_key + " not in vcf_variants_dictionary.")
+  elif len(vcf_variants_dictionary[variant_key]) > 1:
+    sys.exit("Variant " + variant_key + " has more than one VCF record.")
   else:
-    sys.exit("Variant allele of " + variant + " is not 0 or 1.")
+    barcode_supports_this_allele = "No Coverage"
+    this_variant = vcf_variants_dictionary[variant_key][0]
+    n_alleles = len(this_variant.return_Molecules())
+    for i in range(n_alleles):
+      if barcode in this_variant.return_Molecules()[i]:
+        if i == 0:
+          barcode_supports_this_allele = "REF"
+        if i == 1:
+          barcode_supports_this_allele = "ALT"
+    return(barcode_supports_this_allele)
 
-  barcodes_allele0 = this_variant.return_Molecules()[0].keys()
-  barcdoes_allele1 = this_variant.return_Molecules()[1].keys()
-
-  for bx in barcode_list:
-    if bx in barcodes_allele0 and variant_allele == 0:
-      alt_supported = True
-    if bx in barcodes_allele0 and variant_allele == 1:
-      ref_supported = True
-    if bx in barcodes_allele1 and variant_allele == 1:
-      alt_supported = True
-    if bx in barcodes_allele1 and variant_allele == 0:
-      ref_supported = True
-    if alt_supported and ref_supported:
-      sys.exit("Both REF and ALT of " + variant_key + " supported by bx " + bx)
-    elif alt_supported and not ref_supported:
-      coverage_vector.append(1)
-    elif ref_supported and not alt_supported:
-      coverage_vector.append(0)
+def return_haplotype_supported_by_barcode(barcode, variant_key, vcf_variants_dictionary):
+  if variant_key not in vcf_variants_dictionary:
+    sys.exit("Variant " + variant_key + " not in vcf_variants_dictionary.")
+  elif len(vcf_variants_dictionary[variant_key]) > 1:
+    sys.exit("Variant " + variant_key + " has more than one VCF record.")
+  else:
+    this_variant = vcf_variants_dictionary[variant_key][0]
+    if this_variant.return_IsPhasedHeterozygote():
+      barcode_supports_this_allele = return_allele_supported_by_barcode(barcode, variant_key, vcf_variants_dictionary)
+      if this_variant.return_Genotype()[0] == barcode_supports_this_allele:
+        barcode_supports_this_haplotype = "H1"
+      elif this_variant.return_Genotype()[2] == barcode_supports_this_allele:
+        barcode_supports_this_haplotype = "H2"
+      elif "No Coverage" == barcode_supports_this_allele:
+        barcode_supports_this_haplotype = "No Coverage"
     else:
-      coverage_vector.append(-1)
-
-  if len(coverage_vector) == len(barcode_list):
-    return(coverage_vector)
-  else:
-    sys.exit("Coverage vector is different length than barcode vector.")
+      barcode_supports_this_haplotype = "Not Phased Heterozygote"
+    return(barcode_supports_this_haplotype)
 
 def create_coverage_dictionary(variant_key, vcf_variants_dictionary):
 
   bx_supporting_variants = return_barcodes_supporting_variant(variant_key, vcf_variants_dictionary)
   variants_covered = return_variants_covered_by_barcodes(bx_supporting_variants, variant_key, vcf_variants_dictionary)
-  #coverage_dict = {}
-  #coverage_dict["barcodes"].extend(bx_supporting_variants)
   
-  #for variant in variants_covered:
-  #  coverage_dict[variant] = return_coverage_vector_for_variant()
-
-  #return(coverage_dict)
+  coverage_dictionary = {}
+  for bx in bx_supporting_variants:
+    for var in variants_covered:
+      this_variant_key = var.return_VariantKey()
+      this_coverage_key = bx + "--" + this_variant_key
+      coverage_dictionary[this_coverage_key] = [bx, this_variant_key]
+      allele_supported_by_barcode = return_allele_supported_by_barcode(bx, this_variant_key, vcf_variants_dictionary)
+      if not allele_supported_by_barcode == "NA":
+        haplotype_supported_by_barcode = return_haplotype_supported_by_barcode(bx, this_variant_key, vcf_variants_dictionary)
+      else:
+        haplotype_supported_by_barcode = "NA"
+      if var.return_Filter() == []:
+        filter_string = "PASS"
+      else:
+        filter_string = ", ".join(var.return_Filter())
+      coverage_dictionary[this_coverage_key].extend([
+        allele_supported_by_barcode, 
+        haplotype_supported_by_barcode, 
+        var.return_IsPhasedHeterozygote(),
+        var.return_Chromosome(), 
+        var.return_Position(), 
+        var.return_Genotype(), 
+        filter_string])
+  return(coverage_dictionary)
 
 def write_coverage_dictionary(coverage_dictionary, output_file_path):
 
-  variants = sorted([ x for x in coverage_dictionary.keys() if x != "barcodes" ])
-  column_headers = ["barcodes"]
-  column_headers.extend(variants)
-  n_bx = len(coverage_dictionary["barcodes"])
-
   output_file = open(output_file_path, "w")
-
-  #output_file.write("\t".join(column_headers) + "\n")
-  print("\t".join(column_headers))
-
-  for i in range(n_bx):
-    write_line = []
-    for var in column_headers:
-      write_line.append(coverage_dictionary[var][i])
-    #output_file.write("\t".join(write_line) + "\n")
-    print("\t".join(write_line))
+  output_file.write('\t'.join(["Barcode", "Variant", "Allele", "Haplotype", 
+    "Phased_Heterozygote", "Chromosome", "Position", "Genotype", "Filter"]) + '\n')
+  for x in coverage_dictionary:
+    output_file.write('\t'.join([str(x) for x in coverage_dictionary[x]]) + '\n')
 
   output_file.close()
 
@@ -158,7 +161,7 @@ def main(args):
   # write output and close files
   os.makedirs(args.output_directory, exist_ok = True)
   output_file_path = os.path.join(args.output_directory, args.output_prefix + ".FILENAME.tsv")
-  #write_coverage_dictionary(coverage_dictionary, output_file_path)  
+  write_coverage_dictionary(coverage_dictionary, output_file_path)
   
   # write results to output file here
   
