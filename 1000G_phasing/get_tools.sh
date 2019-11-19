4c93313ae185034d33a7f48429f7e49f133de79c
envest <stevenmasonfoltz@gmail.com>
2019-11-19 14:42:05 -0600

    Describe download of 1000G demographic data

--- 1000G_phasing/get_tools.sh
+++ 1000G_phasing/get_tools.sh
@@ -14,4 +14,5 @@ wget http://faculty.washington.edu/browning/refined-ibd/refined-ibd.16May19.ad5.
 # Get merge IBD
 wget https://faculty.washington.edu/browning/refined-ibd/merge-ibd-segments.16May19.ad5.jar -P $tools_dir
 
-wget http://bochet.gcc.biostat.washington.edu/beagle/1000_Genomes_phase3_v5a/sample_info/integrated_call_samples_v3.20130502.ALL.panel
+# Sample demographic information
+wget http://bochet.gcc.biostat.washington.edu/beagle/1000_Genomes_phase3_v5a/sample_info/integrated_call_samples_v3.20130502.ALL.panel -P $tools_dir
