c9eb2a51d58c221ea95ea84f1ee749a011f625dc
Steven Foltz <stevenmasonfoltz@gmail.com>
2020-01-17 10:09:12 -0600

    Include 1000G samples

--- paper/analysis/01_overview.R
+++ paper/analysis/01_overview.R
@@ -59,6 +59,7 @@ dir.create(supp, recursive = TRUE, showWarnings = FALSE)
   multiplier_tbl <- tibble(category = data_columns, data_multiplier, data_multiplier_label)
 
   plot_df <- lr_summary_tbl %>%
+    bind_rows(lr_summary_1000G_tbl) %>%
     gather(data_columns, key = "category", value = "result") %>%
     select(-c(vcf_column, cnv_maf_status, longranger_version)) %>%
     mutate(normal_sample = case_when(timepoint == "Normal" ~ "Normal",
@@ -67,8 +68,8 @@ dir.create(supp, recursive = TRUE, showWarnings = FALSE)
 
   ggplot(plot_df, aes(x = normal_sample, y = result*data_multiplier)) +
     geom_violin(show.legend = FALSE, draw_quantiles = .5) +
-    geom_jitter(aes(color = my_color_100),
-                height = 0, width = 0.25, shape = 16, show.legend = FALSE) +
+    geom_jitter(aes(color = my_color_100, shape = my_shape),
+                height = 0, width = 0.25, show.legend = FALSE) + #, shape = 16) +
     geom_label(data = plot_df %>%
                  select(category, data_multiplier_label, normal_sample) %>%
                  filter(normal_sample == "Tumor") %>% unique(),
@@ -76,6 +77,7 @@ dir.create(supp, recursive = TRUE, showWarnings = FALSE)
                y = -Inf, color = "#000000") +
     facet_wrap(~category, ncol = 4, scales = "free_y") +
     scale_color_identity() +
+    scale_shape_identity() +
     labs(x = NULL, y = NULL) +
     theme_bw() +
     theme(axis.ticks.x = element_blank(),
@@ -123,7 +125,8 @@ dir.create(supp, recursive = TRUE, showWarnings = FALSE)
     filter(category %in% highlight_columns)
 
   plot_df <- lr_summary_tbl %>%
-    select(patient, sample, timepoint, n50_linked_reads_per_molecule, n50_phase_block, molecule_length_mean, my_color_100) %>%
+    bind_rows(lr_summary_1000G_tbl) %>%
+    select(patient, sample, timepoint, n50_linked_reads_per_molecule, n50_phase_block, molecule_length_mean, my_color_100, my_shape) %>%
     gather(highlight_columns, key = "category", value = "result") %>%
     mutate(normal_sample = case_when(timepoint == "Normal" ~ "Normal",
                                      TRUE ~ "Tumor")) %>%
@@ -136,11 +139,12 @@ dir.create(supp, recursive = TRUE, showWarnings = FALSE)
 
   ggplot(plot_df, aes(x = normal_sample, y = result*data_multiplier)) +
     geom_violin(show.legend = FALSE, draw_quantiles = .5) +
-    geom_jitter(aes(color = my_color_100),
-                height = 0, width = 0.25, shape = 16, show.legend = FALSE) +
+    geom_jitter(aes(color = my_color_100, shape = my_shape),
+                height = 0, width = 0.25, show.legend = FALSE) + #, shape = 16) +
     expand_limits(y = 0) +
     facet_wrap(~category, nrow = 1, scales = "free_y") +
     scale_color_identity() +
+    scale_shape_identity() +
     labs(x = NULL, y = NULL) +
     theme_bw() +
     theme(axis.ticks.x = element_blank(),
@@ -153,6 +157,7 @@ dir.create(supp, recursive = TRUE, showWarnings = FALSE)
           strip.text = element_text(size = 8),
           plot.margin = unit(c(0,0,0,0), "lines")) +
     ggsave(str_c(main, "top_qc_metrics.pdf"),
+           useDingbats = FALSE,
            height = 2,
            width = 4.25)
 
