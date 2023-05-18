38b0470ee3103ed0b3dbc424259803fe20d9e18a
Steven Foltz <stevenmasonfoltz@gmail.com>
2023-05-18 16:18:40 -0400

    Add plot dfs except last section in 02

--- paper/analysis/02_phase_sets.R
+++ paper/analysis/02_phase_sets.R
@@ -5,6 +5,9 @@
 # Heterozygotes/Coverage in unphased regions
 ################################################################################
 
+data_dir = file.path("data_for_plots/02_overview")
+dir.create(data_dir, showWarnings = FALSE, recursive = TRUE)
+
 main = "figures/02_phase_sets/main/"
 supp = "figures/02_phase_sets/supplementary/"
 
@@ -18,9 +21,11 @@ manuscript_numbers[["02_phase_sets"]] <- list()
 # Phase set N50 by chromosome
 
 {
-  phase_set_summary_tbl %>%
-    filter(timepoint != "Normal") %>%
-    ggplot(aes(x = chromosome, y = N50_broad/1e6)) +
+  phase_set_by_chromosome_plot_df <- phase_set_summary_tbl %>%
+    filter(timepoint != "Normal")
+
+  ggplot(data = phase_set_by_chromosome_plot_df,
+         aes(x = chromosome, y = N50_broad/1e6)) +
     geom_violin(draw_quantiles = 0.5) +
     geom_jitter(aes(color = my_color_100),
                 height = 0, width = 0.25, shape = 16, show.legend = FALSE) +
@@ -37,9 +42,13 @@ manuscript_numbers[["02_phase_sets"]] <- list()
           panel.grid = element_line(size = 0.5),
           strip.background = element_blank(),
           strip.text = element_text(size = 8),
-          plot.margin = unit(c(0,0,0,0), "lines")) +
-    ggsave(str_c(supp, "phase_set_by_chromosome.pdf"),
-           useDingbats = FALSE, width = 7.25, height = 1.5)
+          plot.margin = unit(c(0,0,0,0), "lines"))
+
+  ggsave(str_c(supp, "phase_set_by_chromosome.pdf"),
+         useDingbats = FALSE, width = 7.25, height = 1.5)
+
+  write_tsv(phase_set_by_chromosome_plot_df,
+            file = file.path(data_dir, "phase_set_by_chromosome_plot_df.tsv"))
 
   manuscript_numbers[["02_phase_sets"]][["median_phase_set_length_chr_min"]] <- phase_set_summary_tbl %>% filter(timepoint != "Normal") %>% group_by(chromosome) %>% summarize(median_ps_length = median(N50_broad/1e6)) %>% arrange(median_ps_length) %>% head(1)
   manuscript_numbers[["02_phase_sets"]][["median_phase_set_length_chr_max"]] <- phase_set_summary_tbl %>% filter(timepoint != "Normal") %>% group_by(chromosome) %>% summarize(median_ps_length = median(N50_broad/1e6)) %>% arrange(median_ps_length) %>% tail(1)
@@ -48,14 +57,18 @@ manuscript_numbers[["02_phase_sets"]] <- list()
   manuscript_numbers[["02_phase_sets"]][["chr21_phase_sets_gt20Mb"]] <- phase_set_summary_tbl %>% filter(timepoint != "Normal") %>% filter(chromosome == "chr21", N50_broad > 20*1e6) %>% nrow()
   manuscript_numbers[["02_phase_sets"]][["chr21_phase_sets_gt20Mb_59114"]] <- phase_set_summary_tbl %>% filter(timepoint != "Normal") %>% filter(chromosome == "chr21", N50_broad > 20*1e6, patient == "59114") %>% nrow()
   manuscript_numbers[["02_phase_sets"]][["25183_qc_measures"]] <- lr_summary_tbl %>% filter(timepoint != "Normal") %>% select(sample, molecule_length_mean, mapped_reads) %>% arrange(molecule_length_mean) %>% tail(1)
+
+  rm(phase_set_by_chromosome_plot_df)
 }
 
 # Phase set N50 by sample
 
 {
-  phase_set_summary_tbl %>%
-    filter(timepoint != "Normal") %>%
-    ggplot(aes(x = fct_reorder(display_name, sample_n), y = N50_broad/1e6)) +
+  phase_set_by_sample_plot_df <- phase_set_summary_tbl %>%
+    filter(timepoint != "Normal")
+
+  ggplot(data = phase_set_by_sample_plot_df,
+         aes(x = fct_reorder(display_name, sample_n), y = N50_broad/1e6)) +
     geom_violin(draw_quantiles = 0.5) +
     geom_jitter(aes(color = my_color_100),
                 height = 0, width = 0.25, shape = 16, show.legend = FALSE) +
@@ -72,9 +85,15 @@ manuscript_numbers[["02_phase_sets"]] <- list()
           panel.grid = element_line(size = 0.5),
           strip.background = element_blank(),
           strip.text = element_text(size = 8),
-          plot.margin = unit(c(0,0,0,0), "lines")) +
-    ggsave(str_c(supp, "phase_set_by_sample.pdf"),
-           useDingbats = FALSE, width = 7.25, height = 1.5)
+          plot.margin = unit(c(0,0,0,0), "lines"))
+
+  ggsave(str_c(supp, "phase_set_by_sample.pdf"),
+         useDingbats = FALSE, width = 7.25, height = 1.5)
+
+  write_tsv(phase_set_by_sample_plot_df,
+            file = file.path(data_dir, "phase_set_by_sample_plot_df.tsv"))
+
+  rm(phase_set_by_sample_plot_df)
 }
 
 # coverage of phase sets
@@ -117,16 +136,16 @@ if (FALSE) {
 
 }
 
-
-
 # Phase set length by deletion
 {
-  phase_sets_tbl %>%
+  phase_set_deletion_plot_df <- phase_sets_tbl %>%
     filter(sample == "27522_1", length_variants > 1e3) %>%
     mutate(deleted = case_when(chromosome == "chr13" ~ "chr13",
                                chromosome == "chr22" ~ "chr22",
-                               TRUE ~ "Others")) %>%
-    ggplot(aes(x = deleted, y = length_variants/1e6)) +
+                               TRUE ~ "Others"))
+
+  ggplot(data = phase_set_deletion_plot_df,
+         aes(x = deleted, y = length_variants/1e6)) +
     geom_violin(draw_quantiles = 0.5) +
     geom_jitter(aes(color = my_color_100), alpha = 0.25, shape = 16,
                 height = 0, width = 0.25, show.legend = FALSE) +
@@ -144,19 +163,25 @@ if (FALSE) {
           panel.grid.minor = element_blank(),
           strip.background = element_blank(),
           strip.text = element_text(size = 8),
-          plot.margin = unit(c(0,0,0,0), "lines")) +
-    ggsave(str_c(supp, "phase_set_deletion.pdf"),
+          plot.margin = unit(c(0,0,0,0), "lines"))
+
+  ggsave(str_c(supp, "phase_set_deletion.pdf"),
            useDingbats = FALSE, width = 2, height = 1.5)
+
+  write_tsv(phase_set_deletion_plot_df,
+            file = file.path(data_dir, "phase_set_deletion_plot_df.tsv"))
 
   manuscript_numbers[["02_phase_sets"]][["27522_1_chr13_chr22_phase_set_lengths"]] <- phase_set_summary_tbl %>% filter(sample == "27522_1", chromosome %in% c("chr13", "chr22")) %>% select(sample, N50_broad, chromosome)
   manuscript_numbers[["02_phase_sets"]][["27522_1_overall_phase_set_lengths"]] <- phase_set_summary_tbl %>% group_by(sample) %>% summarize(median_n50_ps_length = median(N50_broad)) %>% filter(sample == "27522_1")
+
+  rm(phase_set_deletion_plot_df)
 }
 
 # Phase sets genome coverage
 {
-  plot_df <- phase_sets_tbl %>% filter(!is.na(length_variants),
-                                       length_variants > 0,
-                                       timepoint != "Normal") %>%
+  phase_set_genome_coverage_plot_df <- phase_sets_tbl %>% filter(!is.na(length_variants),
+                                                                 length_variants > 0,
+                                                                 timepoint != "Normal") %>%
     mutate(length_group = plyr::round_any(length_variants,
                                           accuracy = 1e6,
                                           f = ceiling)/1e6) %>%
@@ -168,7 +193,8 @@ if (FALSE) {
     mutate(length_label = str_c(length_group - 1, "-", length_group)) %>%
     mutate(length_factor = factor(length_label, levels = length_label, ordered = TRUE))
 
-  ggplot(plot_df, aes(x = length_factor, y = total_length/1e9)) +
+  ggplot(phase_set_genome_coverage_plot_df,
+         aes(x = length_factor, y = total_length/1e9)) +
     geom_col(fill = "#bdbdbd") +
     geom_text(aes(label = str_c("", count)), angle = -90,
               hjust = 1, vjust = 0.5,  nudge_y = 0.05, size = 2) +
@@ -187,19 +213,23 @@ if (FALSE) {
           strip.background = element_blank(),
           strip.text = element_text(size = 8),
           plot.margin = unit(c(0,0,0,0), "lines")
-    ) +
-    ggsave(str_c(supp, "phase_set_genome_coverage.pdf"),
-           useDingbats = FALSE, width = 7.25, height = 1.75)
+    )
 
-  manuscript_numbers[["02_phase_sets"]][["total_genome_coverage_Gb"]] <- plot_df %>% pull(total_length) %>% sum()/1e9
-  manuscript_numbers[["02_phase_sets"]][["average_genome_coverage_Gb"]] <- plot_df %>% pull(total_length) %>% sum()/(23*1e9)
-  manuscript_numbers[["02_phase_sets"]][["length_group_most_ps"]] <- plot_df %>% group_by(length_factor) %>% summarize(count = count, total = sum(plot_df$count), count_pct = 100*count/total) %>% arrange(count_pct) %>% tail(1)
-  manuscript_numbers[["02_phase_sets"]][["0-1_Mb_percentage_of_coverage"]] <- plot_df %>% group_by(length_factor) %>% summarize(total_length = total_length/1e9, total_length_all = sum(plot_df$total_length)/1e9, total_length_pct = 100*total_length/total_length_all) %>% filter(length_factor == "0-1")
-  manuscript_numbers[["02_phase_sets"]][["1-2_Mb_percentage_of_coverage"]] <- plot_df %>% group_by(length_factor) %>% summarize(count = count, total_length = total_length/1e9, total_length_all = sum(plot_df$total_length)/1e9, total_length_pct = 100*total_length/total_length_all) %>% filter(length_factor == "1-2")
-  manuscript_numbers[["02_phase_sets"]][["n_phase_sets_gt_30Mb"]] <- plot_df %>% filter(length_group >= 31) %>% pull(count) %>% sum()
-  manuscript_numbers[["02_phase_sets"]][["longest_ps"]] <- plot_df %>% arrange(length_group) %>% tail(1)
+  ggsave(str_c(supp, "phase_set_genome_coverage.pdf"),
+         useDingbats = FALSE, width = 7.25, height = 1.75)
 
-  rm(plot_df)
+  write_tsv(phase_set_genome_coverage_plot_df,
+            file = file.path(data_dir, "phase_set_genome_coverage_plot_df.tsv"))
+
+  manuscript_numbers[["02_phase_sets"]][["total_genome_coverage_Gb"]] <- phase_set_genome_coverage_plot_df %>% pull(total_length) %>% sum()/1e9
+  manuscript_numbers[["02_phase_sets"]][["average_genome_coverage_Gb"]] <- phase_set_genome_coverage_plot_df %>% pull(total_length) %>% sum()/(23*1e9)
+  manuscript_numbers[["02_phase_sets"]][["length_group_most_ps"]] <- phase_set_genome_coverage_plot_df %>% group_by(length_factor) %>% summarize(count = count, total = sum(phase_set_genome_coverage_plot_df$count), count_pct = 100*count/total) %>% arrange(count_pct) %>% tail(1)
+  manuscript_numbers[["02_phase_sets"]][["0-1_Mb_percentage_of_coverage"]] <- phase_set_genome_coverage_plot_df %>% group_by(length_factor) %>% summarize(total_length = total_length/1e9, total_length_all = sum(phase_set_genome_coverage_plot_df$total_length)/1e9, total_length_pct = 100*total_length/total_length_all) %>% filter(length_factor == "0-1")
+  manuscript_numbers[["02_phase_sets"]][["1-2_Mb_percentage_of_coverage"]] <- phase_set_genome_coverage_plot_df %>% group_by(length_factor) %>% summarize(count = count, total_length = total_length/1e9, total_length_all = sum(phase_set_genome_coverage_plot_df$total_length)/1e9, total_length_pct = 100*total_length/total_length_all) %>% filter(length_factor == "1-2")
+  manuscript_numbers[["02_phase_sets"]][["n_phase_sets_gt_30Mb"]] <- phase_set_genome_coverage_plot_df %>% filter(length_group >= 31) %>% pull(count) %>% sum()
+  manuscript_numbers[["02_phase_sets"]][["longest_ps"]] <- phase_set_genome_coverage_plot_df %>% arrange(length_group) %>% tail(1)
+
+  rm(phase_set_genome_coverage_plot_df)
 }
 
 # Phase sets by sample
@@ -218,14 +248,14 @@ if (FALSE) {
     mutate(phase_set_color = case_when(event_number == 0 ~ my_color_100,
                                        TRUE ~ my_color_50))
 
-  plot_df <- phase_sets_tbl_long %>% bind_rows(phase_sets_tbl_short) %>%
+  chr13_chr22_phase_sets_plot_df <- phase_sets_tbl_long %>% bind_rows(phase_sets_tbl_short) %>%
     mutate(chr_num = as.numeric(chromosome)) %>%
     mutate(sample_num = as.numeric(display_name))
 
   sample_names <- c("27522 (P)", "27522 (Rem)")
   n_samples <- length(sample_names)
 
-  ggplot(plot_df,
+  ggplot(chr13_chr22_phase_sets_plot_df,
          aes(xmin = start/1e6, xmax = end/1e6,
              ymin = sample_num - 0.4, ymax = sample_num + 0.4)) +
     geom_rect(aes(fill = "#ffffff"), show.legend = FALSE) +
@@ -241,29 +271,35 @@ if (FALSE) {
     labs(x = "Position (Mb)") +
     theme_bw() +
     theme(#panel.grid.major.y = element_blank(),
-          panel.grid.minor.y = element_blank(),
-          panel.border = element_blank(),
-          panel.background = element_blank(),
-          axis.ticks.x = element_blank(),
-          axis.ticks.y = element_blank(),
-          axis.title = element_text(size = 8),
-          axis.text.y = element_text(size = 8),
-          axis.text.x = element_text(size = 8),
-          strip.background = element_blank(),
-          strip.text = element_text(size = 8),
-          plot.margin = unit(c(0,0,0,0), "lines")) +
-    ggsave(str_c(supp, "chr13_chr22_phase_sets.pdf"),
-                 height = 2.65, width = 5, useDingbats = FALSE)
+      panel.grid.minor.y = element_blank(),
+      panel.border = element_blank(),
+      panel.background = element_blank(),
+      axis.ticks.x = element_blank(),
+      axis.ticks.y = element_blank(),
+      axis.title = element_text(size = 8),
+      axis.text.y = element_text(size = 8),
+      axis.text.x = element_text(size = 8),
+      strip.background = element_blank(),
+      strip.text = element_text(size = 8),
+      plot.margin = unit(c(0,0,0,0), "lines"))
 
-  rm(phase_sets_tbl_short, phase_sets_tbl_long, plot_df, sample_names, n_samples)
+  ggsave(str_c(supp, "chr13_chr22_phase_sets.pdf"),
+         height = 2.65, width = 5, useDingbats = FALSE)
+
+  write_tsv(chr13_chr22_phase_sets_plot_df,
+            file = file.path(data_dir, "chr13_chr22_phase_sets_plot_df.tsv"))
+
+  rm(chr13_chr22_phase_sets_plot_df, phase_sets_tbl_short, phase_sets_tbl_long, plot_df, sample_names, n_samples)
 }
 
 # phase set length vs. n heterozygous SNPs
 {
-  phase_sets_tbl %>%
+  phase_set_length_vs_variants_plot_df <- phase_sets_tbl %>%
     filter(n_variants_total > 0,
-           timepoint != "Normal") %>%
-    ggplot(aes(x = length_variants/1e6, y = n_variants_total/1e3)) +
+           timepoint != "Normal")
+
+  ggplot(data = phase_set_length_vs_variants_plot_df,
+         aes(x = length_variants/1e6, y = n_variants_total/1e3)) +
     geom_point(aes(color = my_color_100), shape = 16, show.legend = FALSE) +
     geom_smooth(method = "lm") +
     expand_limits(x = 0) +
@@ -277,16 +313,24 @@ if (FALSE) {
           axis.text.x = element_text(size = 8),
           strip.background = element_blank(),
           strip.text = element_text(size = 8),
-          plot.margin = unit(c(0,0,0,0), "lines")) +
-    ggsave(str_c(supp, "phase_set_length_vs_variants.pdf"),
+          plot.margin = unit(c(0,0,0,0), "lines"))
+
+  ggsave(str_c(supp, "phase_set_length_vs_variants.pdf"),
            width = 7.5, height = 7.5, useDingbats = FALSE)
+
+  write_tsv(phase_set_length_vs_variants_plot_df,
+            file = file.path(data_dir, "phase_set_length_vs_variants_plot_df.tsv"))
+
+  rm(phase_set_length_vs_variants_plot_df)
 
   manuscript_numbers[["02_phase_sets"]][["phase_set_length_vs_n_phased_variants"]] <- summary(lm(n_variants_total ~ length_variants, data = phase_sets_tbl %>% mutate(phase_set_length_Mb = length_variants/1e6, n_variants_total_per1000 = n_variants_total/1000) %>% filter(timepoint != "Normal")))
 
-  phase_sets_tbl %>%
+  phase_set_length_vs_variants.27522_1_plot_df <- phase_sets_tbl %>%
     filter(n_variants_total > 0,
-           sample == "27522_1") %>%
-    ggplot(aes(x = length_variants/1e6, y = n_variants_total/1e3)) +
+           sample == "27522_1")
+
+  ggplot(data = phase_set_length_vs_variants.27522_1_plot_df,
+         aes(x = length_variants/1e6, y = n_variants_total/1e3)) +
     geom_point(aes(color = my_color_100), shape = 16, show.legend = FALSE) +
     geom_smooth(method = "lm") +
     expand_limits(x = 0) +
@@ -302,9 +346,16 @@ if (FALSE) {
           axis.text.x = element_text(size = 8),
           strip.background = element_blank(),
           strip.text = element_text(size = 8),
-          plot.margin = unit(c(0,0,0,0), "lines")) +
-    ggsave(str_c(supp, "phase_set_length_vs_variants.27522_1.pdf"),
+          plot.margin = unit(c(0,0,0,0), "lines"))
+
+  ggsave(str_c(supp, "phase_set_length_vs_variants.27522_1.pdf"),
            width = 7.5, height = 7.5, useDingbats = FALSE)
+
+  write_tsv(phase_set_length_vs_variants.27522_1_plot_df,
+            file = file.path(data_dir, "phase_set_length_vs_variants.27522_1_plot_df.tsv"))
+
+  rm(phase_set_length_vs_variants.27522_1_plot_df)
+
 }
 
 
